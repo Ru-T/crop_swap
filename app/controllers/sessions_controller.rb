d20ffd19f0b68c9d12163f8953e83a4dd17d61d3
Ru-T <rwajnberg@gmail.com>
2015-11-04 09:44:09 -0500

    Set up session controller, change routes for session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,58 +1,21 @@
 class SessionsController < ApplicationController
-  before_action :set_session, only: [:show, :edit, :update, :destroy]
 
-  # GET /sessions
-  def index
-    @sessions = Session.all
-  end
-
-  # GET /sessions/1
-  def show
-  end
-
-  # GET /sessions/new
   def new
-    @session = Session.new
   end
 
-  # GET /sessions/1/edit
-  def edit
-  end
-
-  # POST /sessions
   def create
-    @session = Session.new(session_params)
-
-    if @session.save
-      redirect_to @session, notice: 'Session was successfully created.'
+    u = User.find_by_email(params[:email])
+    if u && u.authenticate(params[:password])
+      redirect_to users_path, notice: 'You have successfully logged in'
+      session[:user_id] = u.id
     else
-      render :new
+      redirect_to session_new_path, notice: 'Invalid username/password'
     end
   end
 
-  # PATCH/PUT /sessions/1
-  def update
-    if @session.update(session_params)
-      redirect_to @session, notice: 'Session was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /sessions/1
   def destroy
-    @session.destroy
-    redirect_to sessions_url, notice: 'Session was successfully destroyed.'
+    session[:user_id] = nil
+    redirect_to session_new_path, notice: 'You have logged out'
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_session
-      @session = Session.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def session_params
-      params.require(:session).permit(:new, :create, :destroy)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :sessions
+  root 'users#index'
+
   resources :crops
-  root 'users#new'
   resources :users
+
+  get 'session/new'
+  post 'session/create'
+  delete 'session/destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
