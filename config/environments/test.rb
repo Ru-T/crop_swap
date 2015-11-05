e0e89a2e62299f8b84296f9e572e6840533f030f
Ru-T <rwajnberg@gmail.com>
2015-11-05 14:15:57 -0500

    Add config to test.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+          api_key: '<mailgun api key>',
+          domain: '<mailgun domain>'
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
