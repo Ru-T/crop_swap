7cb127cd48eae53c8d4a03393bffb9a44419ba74
Ru-T <rwajnberg@gmail.com>
2015-11-05 09:24:28 -0500

    Edits to nav.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
   def create
     u = User.find_by_email(params[:email])
     if u && u.authenticate(params[:password])
-      redirect_to crops_pathat
+      redirect_to crops_path
       session[:user_id] = u.id
     else
       redirect_to session_new_path, notice: 'Invalid username/password'
