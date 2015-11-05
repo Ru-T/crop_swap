3259bccb0dc7687dea07390553a63d97a4c7d61a
Ru-T <rwajnberg@gmail.com>
2015-11-05 14:21:45 -0500

    Add api key and domain from Mailgun into config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-          api_key: '<mailgun api key>',
-          domain: '<mailgun domain>'
+          api_key: 'a28a3db62959f15c5860b5833668a8ec',
+          domain: 'https://api.mailgun.net/v3/sandbox85b185dc6fa44a7bbb05e2ce19beca69.mailgun.org'
   }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
