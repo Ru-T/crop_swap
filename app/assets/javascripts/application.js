965167c3b7edfaa4da16216dd145b0543fc8d853
ericquayson <ericquayson@github.com>
2015-11-05 14:50:12 -0500

    added the iron yard coordinates and placed a marker at that location

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//
+// $(document).ready(function() {
+//   var map = L.map('map', {
+//     center: [35.98, 78.90],
+//     zoom: 13
+//   });
+//   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/search?query=durham%20nc#map=12/35.9940/-78.8986"',
+//       maxZoom: 18
+//   }).addTo(map);
+//
+//   //var marker = L.maker([35.98, 78.90]).addTo(map);
+//
+// });
 
-$(document).ready(function() {
-  var map = L.map('map', {
-    center: [35.98, 78.90],
-    zoom: 13
-  });
-  
-});
+$(document).ready(function () {
+  var map = L.map('map')
+  .setView([35.98, 78.90], 13);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'ebaiden.cigmgqf43027nurkrnufjii94',
+    accessToken: 'pk.eyJ1IjoiZWJhaWRlbiIsImEiOiJjaWdtbHozMjEwMDA3bjlrb2gwaTY1ZThqIn0.w4-Mcgb3JXeCNLjFvigYDg'
+  }).addTo(map);
+
+  var marker = L.marker([35.994, -78.898]).addTo(map);
+
+    map.panTo(new L.LatLng(35.994, -78.898));
+}); // end of the function
