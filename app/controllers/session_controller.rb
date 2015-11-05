31ccd946a8765f02e943cc7baef9aeaad7e58ad9
Ru-T <rwajnberg@gmail.com>
2015-11-05 08:53:09 -0500

    Remove login and logout notice alerts.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
   def create
     u = User.find_by_email(params[:email])
     if u && u.authenticate(params[:password])
-      redirect_to crops_path, notice: 'You have successfully logged in'
+      redirect_to crops_pathat
       session[:user_id] = u.id
     else
       redirect_to session_new_path, notice: 'Invalid username/password'
@@ -15,7 +15,7 @@ class SessionController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to session_new_path, notice: 'You have logged out'
+    redirect_to session_new_path
   end
 
 end
