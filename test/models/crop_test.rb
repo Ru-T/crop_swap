f8125a9a54dcd38b4993a3e7d6ed5b2cf6a4055e
Ru-T <rwajnberg@gmail.com>
2015-11-09 16:50:17 -0500

    Pass tests for ripe and expire methods on crop.

--- test/models/crop_test.rb
+++ test/models/crop_test.rb
@@ -5,39 +5,39 @@ class CropTest < ActiveSupport::TestCase
   test "crop is ripe" do
     c1 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-8", expires_on: "2015-11-20")
+      weight: 3, ripe_on: Date.today - 2.days, expires_on: Date.today + 8.days)
 
     assert c1.is_ripe?
 
     c2 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-22", expires_on: "2015-11-30")
+      weight: 3, ripe_on: Date.today + 7.days, expires_on: Date.today + 10.days)
 
-    assert c2.is_ripe?
+    refute c2.is_ripe?
 
     c3 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-1", expires_on: "2015-11-3")
+      weight: 3, ripe_on: Date.today - 3.days, expires_on: Date.today - 2.days)
 
-    assert c3.is_ripe?
+    refute c3.is_ripe?
   end
 
   test "crop about to expire" do
     c1 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-8", expires_on: "2015-11-11")
+      weight: 3, ripe_on: Date.today - 2.days, expires_on: Date.today + 2.days)
 
     assert c1.about_to_expire?
 
     c2 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-01", expires_on: "2015-11-15")
+      weight: 3, ripe_on: Date.today - 7.days, expires_on: Date.today - 2.days)
 
-    assert c2.about_to_expire?
+    refute c2.about_to_expire?
 
     c3 = Crop.create!(user_id: 1, crop_type_id: 1,
       description: "This potato is delectable. You want to trade me for this beet.",
-      weight: 3, ripe_on: "2015-11-1", expires_on: "2015-11-3")
+      weight: 3, ripe_on: Date.today + 2.days, expires_on: Date.today + 5.days)
 
     refute c3.about_to_expire?
   end
