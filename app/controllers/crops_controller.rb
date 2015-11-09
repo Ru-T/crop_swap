ad0e56bb84f057f256e8ceb819a3d1cbbe54b28f
Ru-T <rwajnberg@gmail.com>
2015-11-09 08:42:20 -0500

    Style crop index page with cards.

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -57,6 +57,7 @@ class CropsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def crop_params
-      params.require(:crop).permit(:user_id, :description, :weight, :crop_pic, :crop_type_id, :ripe_on, :expires_on)
+      params.require(:crop).permit(:user_id, :description, :weight, :crop_pic,
+      :crop_type_id, :ripe_on, :expires_on)
     end
 end
