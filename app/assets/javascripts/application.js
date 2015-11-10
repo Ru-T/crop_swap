ee47890281eb1c24baed93e6f7839326e42f5dab
Ru-T <rwajnberg@gmail.com>
2015-11-10 09:18:35 -0500

    Cleanup application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,58 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require leaflet
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
-
-
-
-$(document).ready(function () {
-  var svg = d3.select("svg");
-
-  var g = svg.append("g");
-  g.attr("transform", "translate(100, 50)");
-
-  var gx = g.append("g")
-  gx.attr("transform", "translate(0,400)");
-
-  // var legend =  legend = svg.append("g")
-  //   .attr("class","legend")
-  //   .attr("transform","translate(50,30)")
-  //   .style("font-size","12px")
-  //   .call(d3.legend);
-  //
-//   //
-//   var color_hash = {  0 : ["apple", "green"],
-//   1 : ["mango", "orange"],
-//   2 : ["cherry", "red"]
-// }
-
-
-  var x = d3.scale.linear()
-      .domain([0, 1000000])
-      .range([0, 850]);
-  var y = d3.scale.linear()
-      .domain([0, 2000000])
-      .range([400, 0]);
-
-  var y_axis = d3.svg.axis().scale(y).orient("left").ticks(4);
-  g.call(y_axis);
-
-
-  var x_axis = d3.svg.axis().scale(x).orient("bottom").ticks(5).tickFormat(d3.format("d"));
-  gx.call(x_axis);
-
-
-  d3.csv("avgexpenses.csv", function(data) {
-
-    g.selectAll("circle")
-        .data(data)
-      .enter().append("circle")
-        .attr("cx", function(d) {return x(d["Earnings_of_farms"]);} )
-        .attr("cy", function(d) {return y(+d["average_farm_production_expenses"]);} )
-        .attr("r", 10);
-  });
-
-});
 
 
 // Leaftlet/map code
