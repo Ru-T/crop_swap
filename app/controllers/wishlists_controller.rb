9007f3ea9756f127b95b67f9df316b8bdbf4bc21
Ru-T <rwajnberg@gmail.com>
2015-11-14 17:24:32 -0500

    Remove jbuilder nonsense from wishlist controller.

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -2,13 +2,11 @@ class WishlistsController < ApplicationController
   before_action :set_wishlist, only: [:show, :edit, :update, :destroy]
 
   # GET /wishlists
-  # GET /wishlists.json
   def index
     @wishlists = Wishlist.all
   end
 
   # GET /wishlists/1
-  # GET /wishlists/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class WishlistsController < ApplicationController
   end
 
   # POST /wishlists
-  # POST /wishlists.json
   def create
     @wishlist = Wishlist.new(wishlist_params)
-
-    respond_to do |format|
       if @wishlist.save
-        format.html { redirect_to @wishlist, notice: 'Wishlist was successfully created.' }
-        format.json { render :show, status: :created, location: @wishlist }
+        redirect_to @wishlist, notice: 'Wishlist was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @wishlist.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
 
   # PATCH/PUT /wishlists/1
-  # PATCH/PUT /wishlists/1.json
   def update
-    respond_to do |format|
-      if @wishlist.update(wishlist_params)
-        format.html { redirect_to @wishlist, notice: 'Wishlist was successfully updated.' }
-        format.json { render :show, status: :ok, location: @wishlist }
-      else
-        format.html { render :edit }
-        format.json { render json: @wishlist.errors, status: :unprocessable_entity }
-      end
+    if @wishlist.update(wishlist_params)
+      redirect_to @wishlist, notice: 'Wishlist was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /wishlists/1
-  # DELETE /wishlists/1.json
   def destroy
     @wishlist.destroy
-    respond_to do |format|
-      format.html { redirect_to wishlists_url, notice: 'Wishlist was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to wishlists_url, notice: 'Wishlist was successfully destroyed.'
   end
 
   private
