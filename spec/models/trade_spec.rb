e8b29063b3790f1cb13aa843abd1b0d6ebcad794
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 14:17:40 -0500

    Add unit test for rejecting other trades.

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
-  let(:trade) { create(:trade) }
+  let(:user) { create(:user) }
+  let(:crop) { create(:crop, user: user) }
+  let(:trade) { create(:trade, crop: crop) }
+  let(:trade2) { create(:trade, crop: crop) }
+  let(:trade3) { create(:trade, crop: crop) }
 
-  describe "#reject" do
+  describe "#reject!" do
     it "marked the trade as rejected" do
       trade.update(accepted: true)
       trade.reject!
       expect(trade.accepted).to eq false
+    end
+  end
+
+  describe "#reject_other_trades" do
+    it "marks all other trades as rejected when one is accepted" do
+      trade.update(accepted: true)
+      trade.reject_other_trades
+      expect(trade2.accepted).to eq false
+      expect(trade3.accepted).to eq false
+      expect(trade2.message_response). to eq "Sorry - another swap was chosen this time."
+      expect(trade3.message_response). to eq "Sorry - another swap was chosen this time."
     end
   end
 end
