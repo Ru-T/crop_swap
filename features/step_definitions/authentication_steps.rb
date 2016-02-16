38681a68c0c688c12f5ac3589b24c8dcbfe86b83
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 16:40:39 -0500

    Add cucumber config and authentication steps.

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,40 @@
+Given(/^I have an account on the site$/) do
+  visit root_path
+  click_on "Sign up"
+  fill_in 'Email', with: 'newestuser@test.com'
+  fill_in 'Password', with: 'password'
+  fill_in 'Password confirmation', with: 'password'
+  click_on 'Sign up'
+end
+
+When(/^I visit the site root path$/) do
+  visit root_path
+end
+
+Then(/^I am presented with a login page$/) do
+  expect(page).to_have content("Login")
+end
+
+When(/^I enter my correct email$/) do
+  fill_in 'Email', with: 'newestuser@test.com'
+end
+
+When(/^I enter my password$/) do
+  fill_in 'Password', with: 'password'
+end
+
+When(/^I click the "([^"]*)" button$/) do |button|
+  click_on button
+end
+
+Then(/^I am redirected to the crops index page$/) do
+  visit crops_path
+end
+
+When(/^I enter the wrong password$/) do
+  fill_in 'Password', with: 'xyz'
+end
+
+Then(/^I am notified that my password is incorrect$/) do
+  expect(page).to have_content("Invalid email or password.")
+end
