40ee8558ef5cdb178cd7e9c2634aeb5825a41870
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 14:39:03 -0500

    Add unit tests for ripe on and about to expire on crops.

--- spec/factories/crops.rb
+++ spec/factories/crops.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     description "This crop is absolutely delicious"
     weight 12
     crop_type_id 3
-    ripe_on Date.today + 10.days
-    expires_on Date.today + 15.days
+    ripe_on Date.today + 1.days
+    expires_on Date.today + 3.days
   end
 end
--- spec/models/crop_spec.rb
+++ spec/models/crop_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Crop, type: :model do
   let(:user) { create(:user) }
-  let(:crop) { create(:crop, user: user) }
+  let(:crop) { create(:crop, user: user, ripe_on: Date.today - 2.days, expires_on: Date.today + 20.days) }
+  let(:crop2) { create(:crop, user: user, ripe_on: Date.today + 5.days, expires_on: Date.today + 30.days) }
+  let(:crop3) { create(:crop, user: user, expires_on: Date.today + 2.days) }
   let(:trade) { create(:trade, crop: crop, accepted: nil) }
   let(:trade2) { create(:trade, crop: crop, accepted: true) }
 
@@ -41,6 +43,20 @@ RSpec.describe Crop, type: :model do
   describe "trades accepted" do
     it "returns trades that are accepted" do
       expect(crop.trades.accepted).to eq [trade2]
+    end
+  end
+
+  describe "#is_ripe" do
+    it "returns true if a crop is ripe and not yet expired" do
+      expect(crop.is_ripe?).to eq true
+      expect(crop2.is_ripe?).to eq false
+    end
+  end
+
+  describe "#about_to_expire" do
+    it "returns true if a crop is 3 days or less from expiring" do
+      expect(crop2.about_to_expire?).to eq false
+      expect(crop3.about_to_expire?).to eq true
     end
   end
 end
