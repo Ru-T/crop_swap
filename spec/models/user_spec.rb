3a5bcc93fde987d24579d7d05adbb125ccdc8434
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 16:18:15 -0500

    Pass all failing specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,23 +7,27 @@ RSpec.describe User, type: :model do
   let(:crop2) { create(:crop, user: user2) }
   let(:trade) { create(:trade, crop: crop, grower: user2, consumer: user) }
   let(:trade2) { create(:trade, crop: crop2, grower: user2) }
+  let(:trade3) { create(:trade, crop: crop, grower: user2, consumer: user, accepted: true) }
 
-  # describe "#has_proposed_trade_for?" do
-  #   it "returns true if user has proposed a trade on the crop" do
-  #     expect(user.has_proposed_trade_for?(crop)).to eq true
-  #     expect(user.has_proposed_trade_for?(crop2)).to eq false
-  #   end
-  # end
-  #
-  # describe "#has_crop_with_pending_trade?" do
-  #   it "returns true if user has crop with pending trade" do
-  #     expect(user2.has_crop_with_pending_trade?).to eq true
-  #   end
-  # end
-  #
-  # describe "#has_crop_without_pending_trade?" do
-  #   it "returns true if user has crop without pending trade" do
-  #     expect(user2.has_crop_with_pending_trade?).to eq false
-  #   end
-  # end
+  describe "#has_proposed_trade_for?" do
+    it "returns trade if user has proposed a trade on the crop" do
+      trade.reload
+      expect(user.has_proposed_trade_for?(crop)).to eq trade
+      expect(user.has_proposed_trade_for?(crop2)).to eq nil
+    end
+  end
+
+  describe "#has_crop_with_pending_trade?" do
+    it "returns true if user has crop with pending trade" do
+      trade.reload
+      expect(user2.has_crop_with_pending_trade?).to eq true
+    end
+  end
+
+  describe "#has_crop_without_pending_trade?" do
+    it "returns true if user has crop without pending trade" do
+      trade3.reload
+      expect(user2.has_crop_without_pending_trade?).to eq false
+    end
+  end
 end
