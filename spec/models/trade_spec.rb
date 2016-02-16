11b2fc65ad086093a7a60fa4162cdf5f7fee5492
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 15:32:28 -0500

    Add unit tests for trade coverage.

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
   let(:user) { create(:user) }
+  let(:user2) { create(:user, email: "consumer1@mail.com") }
+  let(:user3) { create(:user, email: "consumer2@mail.com") }
   let(:crop) { create(:crop, user: user) }
-  let(:trade) { create(:trade, crop: crop, accepted: true) }
-  let(:trade2) { create(:trade, crop: crop) }
+  let(:trade) { create(:trade, crop: crop, consumer: user2, grower: user, accepted: true) }
+  let(:trade2) { create(:trade, crop: crop, consumer: user3, grower: user) }
 
   describe "#reject!" do
     it "marked the trade as rejected" do
@@ -13,24 +15,24 @@ RSpec.describe Trade, type: :model do
     end
   end
 
-  # describe "#reject_other_trades" do
-  #   it "marks all other trades as rejected when one is accepted" do
-  #     trade.reject_other_trades
-  #     expect(trade2.accepted).to eq false
-  #     expect(trade2.message_response). to eq "Sorry - another swap was chosen this time."
-  #   end
-  # end
+  describe "#reject_other_trades" do
+    it "marks all other trades as rejected when one is accepted" do
+      trade.reject_other_trades
+      trade2.reload
+      expect(trade2.accepted).to eq false
+      expect(trade2.message_response). to eq "Sorry - another swap was chosen this time."
+    end
+  end
 
-  # describe ".user_has_accepted_trades" do
-  #   it "returns true if user has accepted trades" do
+  describe ".user_has_accepted_trades" do
+    it "returns true if user has accepted trades" do
+      expect(Trade.all.user_has_accepted_trades?).to eq true
+    end
+  end
 
-  #   end
-  # end
-
-  # describe ".user_has_rejected_trades" do
-  #   it "returns true if user has rejected trades" do
-
-  #   end
-  # end
-
+  describe ".user_has_rejected_trades" do
+    it "returns true if user has rejected trades" do
+      expect(Trade.all.user_has_rejected_trades?).to eq false
+    end
+  end
 end
