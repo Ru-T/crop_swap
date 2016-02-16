0e503321d54273bd3dc160bc026e829475caf691
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 13:32:51 -0500

    Add back controller files.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    if params[:id]
+      @user = User.find params[:id]
+    else
+      @user = current_user
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :name, :phone_number, :description, :zip_code, :profile_pic)
+    end
+end
