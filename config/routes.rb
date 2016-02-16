deeb66755865e89be96f3c9d0fc5f70a819e1285
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-16 13:53:09 -0500

    Modify session routes for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :trades
   resources :crops, except: [:show]
   resources :users, only: [:show]
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'signup' => 'devise/sessions#create', :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
