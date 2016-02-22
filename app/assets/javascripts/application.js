ae7cfb3f2a7e9b037f6bec66cf8e0455591bc25c
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-22 11:20:07 -0500

    Draper hot mess.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require leaflet
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
